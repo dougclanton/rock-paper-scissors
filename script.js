ef4418e603b9754ef2873bea62a66736dab75711
Doug Clanton <douglas.m.clanton@gmail.com>
2020-01-19 21:26:17 -0500

    Moved Javascript into a seperate file. Added a header buttons and scoreboard to the html file.

---
+++ script.js
@@ -0,0 +1,86 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPlay(){
+    let computerPick = Math.floor(Math.random()*3)+1;
+    if(computerPick == 1){
+        return 'rock';
+    }else if(computerPick == 2){
+        return 'paper';
+    }else{
+        return 'scissors';
+    }
+}
+
+function playerPick(){
+    if(button.value == 'Rock'){
+        return 'rock';
+    }else if(button.value == 'Paper'){
+        return 'paper'
+    }else if(button.value == 'Scissors'){
+        return 'scissors'
+    }else{
+        return 0;
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    while(playerSelection == 'rock'){
+        if(computerSelection == 'rock'){
+            return 'It\'s a tie!'
+        }else if(computerSelection == 'scissors'){
+            return 'You win!'
+            playerScore++
+        }else{
+            return 'You lose!'
+            computerScore++
+        }
+    }
+    while(playerSelection == 'paper'){
+        if(computerSelection == 'rock'){
+            return 'You win!'
+            playerScore++
+        }else if(computerSelection == 'scissors'){
+            return 'You lose!'
+            computerScore++
+        }else{
+            return 'It\'s a tie!'
+        }
+    }
+    while(playerSelection == 'scissors'){
+        if(computerSelection == 'rock'){
+            return 'You lose!'
+            computerScore++
+        }else if(computerSelection == 'scissors'){
+            return 'It\'s a tie!'
+        }else{
+            return 'You win!'
+            playerScore++
+        }
+    }  
+}
+
+function game(){
+    for(let gameCount = 0; gameCount<5; gameCount++){
+        let result = playRound(playerPick(),computerPlay());
+        if(result == "You win!"){
+            alert("Win!");
+            console.log(playerScore);
+            console.log(computerScore);
+        }else if(result == "You lose!"){
+            alert("Loss!");
+            console.log(playerScore);
+            console.log(computerScore);
+        }else if(result == "It\'s a tie!"){
+            alert("Tie!");
+            console.log(playerScore);
+            console.log(computerScore);
+        }else{
+            alert("error");
+        }
+        document.getElementById("playerScore").value = playerScore;
+        document.getElementById("playerScore").value = computerScore;
+    }
+}
+
+game();
